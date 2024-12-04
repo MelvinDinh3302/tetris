bc812d715c63f23fb11da2e57ce40ad56d2a2de3
Melvin Dinh <mdinh@umich.edu>
2024-12-04 04:45:12 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Get all the nav items and links
-  const navItems = document.querySelectorAll("nav ul li");
-  const links = document.querySelectorAll(".links a");
+  // Add highlight when clicked on navbar item
+    if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/") {
 
-  // Add click event to each nav item
-  navItems.forEach((navItem, index) => {
-    navItem.addEventListener("click", () => {
-      // Remove the glow class from all links
-      links.forEach((link) => link.classList.remove("highlight"));
+        const navItems = document.querySelectorAll("nav ul li");
+        const links = document.querySelectorAll(".links a");
 
-      // Add the glow class to the corresponding link
-      if (links[index]) {
-        links[index].classList.add("highlight");
-      }
+        navItems.forEach((navItem, index) => {
+            navItem.addEventListener("click", () => {
+            links.forEach((link) => link.classList.remove("highlight"));
+
+            if (links[index]) {
+                links[index].classList.add("highlight");
+            }
+            });
+        });
+    }
+
+    //Add home button function
+    const homeButton = document.getElementById("homeButton");
+    homeButton.addEventListener("click", function () {
+        window.location.href = "index.html";
     });
-  });
+
+    //Add scroll to top function
+    const scrollButton = document.getElementById("scrollButton");
+    scrollButton.addEventListener("click", function () {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+        });
+    });
+
+    //Add lightbox
+    const thumbnails = document.querySelectorAll("img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImage = lightbox.querySelector(".lightbox-image");
+    const closeBtn = lightbox.querySelector(".close");
+
+    thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", () => {
+        lightboxImage.src = thumbnail.src;
+        lightbox.style.display = "flex";
+        });
+    });
+
+    closeBtn.addEventListener("click", () => {
+        lightbox.style.display = "none";
+    });
+
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox) {
+        lightbox.style.display = "none";
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+        lightbox.style.display = "none";
+        }
+    });
+
 });
+
+
 
